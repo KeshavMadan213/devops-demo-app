07674670caa0d6fac65ac6667a245f30632cf5d2
DevAbhinav3033 <cseabhinav.3033@gmail.com>
2026-08-27 19:02:34 +0530

    Initial commit — Spring Boot CI/CD demo app

---
+++ app/src/main/java/com/sap/training/HelloController.java
@@ -0,0 +1,13 @@
+package com.sap.training;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from SAP DevOps Training!";
+    }
+}
